e414c6bd85c619000af0d169ebac95aed54b6474
Tevin-creator <isaactevin67@gmail..com>
2022-01-12 13:00:26 +0300

    replaced deployed contract address in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 
 const ERC20_DECIMALS = 18
 // const MPContractAddress = "0xFa8C237a75b84d30b3D75b00DeFA4d0Aa7884665"
- const MPContractAddress = "0xC27135D1627407b6CB4860e5D5dA60a7A82bC460"
+ const MPContractAddress = "0xf7066dC2066e598842144D6C4516b977a77fDd55"
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
 
 
